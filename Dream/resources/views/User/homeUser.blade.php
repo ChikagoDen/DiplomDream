c9c8ffc9c7974cf339661339e8495d0cb800546c
ChikagoDen <perepelkin-da@yandex.ru>
2023-04-12 23:29:03 +0600

    modified:   Dream/public/css/home.css 	modified:   Dream/resources/sass/general/home.scss 	modified:   Dream/resources/views/User/homeUser.blade.php

--- Dream/resources/views/User/homeUser.blade.php
+++ Dream/resources/views/User/homeUser.blade.php
@@ -41,35 +41,62 @@
   </head>
 
   <body>
-        <!-- Вот наш главный заголовок, который используется на всех страницах нашего веб-сайта -->
         <header>
-            <div class="logo">
-                <a href="#">
-                    <img src="{{asset('../resources/img/logo.png')}}" alt="Логотип кошка" class="logotype">                    
-                </a>
-            </div>
-            <h2>Мы уверены, что понимание смысла своих снов и знание гороскопа помогают лучше понимать себя и других, принимать важные решения и строить качественные отношения.</h2>
+            <h1 class="hidden">Мы уверены, что понимание смысла своих снов и знание гороскопа помогают лучше понимать себя и других, принимать важные решения и строить качественные отношения. </h1>
+            <a href="#" class="logotype-a">
+                <img src="{{asset('../resources/img/logo.png')}}" alt="Логотип кошка" class="logotype">                    
+            </a>
+            <h2 class="heading">Мы считаем, что понимание смысла снов и гороскопа помогает лучше понимать себя и других, принимать важные решения и улучшать отношения.</h2>
             <a href="#">
-                <div class="authorization">
-                    <p>войти</p>
-                </div>
+                <p class="authorization">войти</p>
             </a>
         </header>
-    
         <nav>
-            <ul>
-            <li><a href="#">Домашняя страница</a></li>
-            <li><a href="#">Наша команда</a></li>
-            <li><a href="#">Проекты</a></li>
-            <li><a href="#">Контакты</a></li>
+            <ul class="basic-navigation">
+                <li><a href="#" class="basic-navigation-button">Домашняя страница</a></li>
+                <li><a href="#" class="basic-navigation-button">Сонник</a></li>
+                <li><a href="#" class="basic-navigation-button">Гороскоп</a></li>
+                <li><a href="#" class="basic-navigation-button">Магазин</a></li>
             </ul>
-    
-            <!-- Форма поиска — это ещё один распространённый нелинейный способ навигации по веб-сайту. -->
-    
-            <form>
-            <input type="search" name="q" placeholder="Search query">
-            <input type="submit" value="Go!">
-            </form>
+            <hr>
+            <h3 class="h3-navigation">Для поиска толкования воспользуйтесь поиском или алфавитным указателем.</h3>
+            <section class="sources-navigation">
+                <form method="get" name="form-navigation">
+                    <input type="search" name="search-form-navigation" placeholder="Ключевое слово из сна">
+                    <input type="submit" value="Искать">
+                </form> 
+                <ul class="letters-navigation">
+                    <li><a href="#" class="button">А</a></li>
+                    <li><a href="#" class="button">Б</a></li>
+                    <li><a href="#" class="button">В</a></li>
+                    <li><a href="#" class="button">Г</a></li>
+                    <li><a href="#" class="button">Д</a></li>
+                    <li><a href="#" class="button">Е</a></li>
+                    <li><a href="#" class="button">Ж</a></li>
+                    <li><a href="#" class="button">З</a></li>
+                    <li><a href="#" class="button">И</a></li>
+                    <li><a href="#" class="button">Й</a></li>
+                    <li><a href="#" class="button">К</a></li>
+                    <li><a href="#" class="button">Л</a></li>
+                    <li><a href="#" class="button">М</a></li>
+                    <li><a href="#" class="button">Н</a></li>
+                    <li><a href="#" class="button">О</a></li>
+                    <li><a href="#" class="button">П</a></li>
+                    <li><a href="#" class="button">Р</a></li>
+                    <li><a href="#" class="button">С</a></li>
+                    <li><a href="#" class="button">Т</a></li>
+                    <li><a href="#" class="button">У</a></li>
+                    <li><a href="#" class="button">Ф</a></li>
+                    <li><a href="#" class="button">Х</a></li>
+                    <li><a href="#" class="button">Ц</a></li>
+                    <li><a href="#" class="button">Ч</a></li>
+                    <li><a href="#" class="button">Ш</a></li>
+                    <li><a href="#" class="button">Щ</a></li>
+                    <li><a href="#" class="button">Э</a></li>
+                    <li><a href="#" class="button">Ю</a></li>
+                    <li><a href="#" class="button">Я</a></li>
+                </ul>                
+            </section>
         </nav>
     
         <!-- Здесь основное содержимое нашей страницы -->
