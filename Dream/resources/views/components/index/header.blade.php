d189d13f0fdc083ecee346935149d4dc16fc671a
ChikagoDen <perepelkin-da@yandex.ru>
2023-05-11 20:16:11 +0600

    modified:   Dream/resources/views/components/index/header.blade.php

--- Dream/resources/views/components/index/header.blade.php
+++ Dream/resources/views/components/index/header.blade.php
@@ -1,7 +1,34 @@
+<style>
+
+
+
+.bod {
+    background: #6ED0F6;
+    color: #fff;
+    font-family: 'Raleway', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    display: flex;
+    position: absolute;
+    width: 100%;
+    height: 70%;
+    left: 0;
+    top: 0;
+    z-index: 2;
+    justify-content: center;
+}
+
+</style>
+
+
+
 <a href="#">
     <img src="{{asset('../resources/img/logo.png')}}" alt="Логотип организации Приснилось" class="logotype">                    
 </a>
 <h1 class="heading">Мы считаем, что понимание смысла снов и гороскопа помогает лучше понимать себя и других, принимать важные решения и улучшать отношения.</h1>
 <a href="#">
     <p class="authorization">войти</p>
+    {{-- <div class="bod">
+
+    </div> --}}
+
 </a>
